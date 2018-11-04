d142ce2afc982ad9a87c7786a89e9bb42ed80f9e
kmcrage <44420720+kmcrage@users.noreply.github.com>
2018-11-04 17:53:26 -0500

    add mpa and bellman backup policies for uct

--- search/__init__.py
+++ search/__init__.py
@@ -1,5 +1,6 @@
 from search.neural_net import NeuralNet
 from search.uct import UCT_search
 from search.crazy import CRAZY_search
-from search.brue import BRUE_search
+from search.mpa_backup import MPA_search
+from search.bellman_backup import Bellman_search
 # from uct.util import softmax, temp_softmax
